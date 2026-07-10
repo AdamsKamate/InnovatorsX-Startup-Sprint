b05808f21cdfbcdea002eb4d68858c3649417625
Adams Kamate <adams.kamate@epitech.eu>
2026-07-10 19:52:46 +0000

    feat: add mock data

---
+++ src/services/mockData.js
@@ -0,0 +1,20 @@
+export const mockData = [
+    {
+      id: 1,
+      title: "A revolutionary AI solution",
+      description: "Automate your daily tasks with artificial intelligence.",
+      status: "Actif"
+    },
+    {
+      id: 2,
+      title: "B2B SaaS Platform",
+      description: "Manage your teams and projects in a single, streamlined workspace.",
+      status: "MVP"
+    },
+    {
+      id: 3,
+      title: "Productivity Tool",
+      description: "Save 2 hours a day with our organizational algorithm.",
+      status: "Bêta"
+    }
+  ];
